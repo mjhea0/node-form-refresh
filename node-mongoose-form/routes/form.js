72e80cadbec61bc04f8e9f10d61df975c8c314fc
Michael Herman <hermanmu@gmail.com>
2014-12-31 02:33:17 -0700

    initial

---
+++ node-mongoose-form/routes/form.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Comment = mongoose.model('comments');
+
+/* GET form. */
+router.get('/', function(req, res) {
+  Comment.find(function(err, comments){
+    console.log(comments)
+    res.render(
+      'form',
+      {title : 'My funky form', comments : comments}
+    );
+  });
+});
+
+/* POST form. */
+router.post('/', function(req, res) {
+  new Comment({title : req.body.comment})
+  .save(function(err, comment) {
+    console.log(comment)
+    res.redirect('form');
+  });
+});
+
+module.exports = router;
---
+++ readme.md
@@ -0,0 +1,1 @@
+Check out the blog post here ->
